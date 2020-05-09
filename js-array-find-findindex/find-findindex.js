2ee8ddf199233bd5d3361077200628bae663af97
Yu Te <yu@pfix.org>
2020-05-08 18:24:40 -0700

    find-findIndex

--- js-array-find-findindex/find-findindex.js
+++ js-array-find-findindex/find-findindex.js
@@ -1,5 +1,7 @@
 /* 
-Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. 
+The function should return the first object with the key of username that matches the string passed to the function. 
+If the object is not found, return undefined. 
 
 const users = [
   {username: 'mlewis'},
@@ -12,10 +14,17 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  let a = usersArray.find(function (value, index, arr) {
+    return value.username == username;
+  });
+
+  return a;
+}
 
 /*
-Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. 
+The function should remove the object from the array. If the object is not found, return undefined. 
 
 const users = [
   {username: 'mlewis'},
@@ -27,4 +36,17 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let i = usersArray.findIndex(function (value, index, arr) {
+    return value.username == username;
+  });
+
+  let retval = undefined;
+
+  if ((i != undefined) && (i >= 0)) {
+    retval = usersArray[i];
+    usersArray.splice(i,1);
+  }
+
+  return retval;
+}
