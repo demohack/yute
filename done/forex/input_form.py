aa0306dcd2f04256a6ba77dce9c7d15bf536186f
Yu <yu@pfix.org>
2022-03-06 07:43:15 -0800

    changed labels for forms

--- done/forex/input_form.py
+++ done/forex/input_form.py
@@ -3,6 +3,6 @@ from wtforms import FloatField, SelectField
 from wtforms.validators import InputRequired, NumberRange
 
 class InputForm(FlaskForm):
-    from_currency_code = SelectField("Converting from", validators=[InputRequired()])
-    to_currency_code = SelectField("Converting to", validators=[InputRequired()])
-    from_amount = FloatField("From amount", validators=[InputRequired()])
+    from_currency_code = SelectField("From", validators=[InputRequired()])
+    to_currency_code = SelectField("To", validators=[InputRequired()])
+    from_amount = FloatField("Amount", validators=[InputRequired()])
--- done/forex/output_form.py
+++ done/forex/output_form.py
@@ -2,11 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField
 
 class OutputForm(FlaskForm):
-    convert_from_country = StringField("Converted From", render_kw={'readonly': True})
-    from_currency_code = StringField("From Currency Code", render_kw={'readonly': True})
-    from_symbol = StringField("From Symbol", render_kw={'readonly': True})
-    from_amount = FloatField("From Amount", render_kw={'readonly': True})
-    convert_to_country = StringField("Converted To", render_kw={'readonly': True})
-    to_currency_code = StringField("To Currency Code", render_kw={'readonly': True})
-    to_symbol = StringField("To Symbol", render_kw={'readonly': True})
-    to_amount = FloatField("To Amount", render_kw={'readonly': True})
+    convert_from_country = StringField("From", render_kw={'readonly': True})
+    from_currency_code = StringField("Currency Code", render_kw={'readonly': True})
+    from_symbol = StringField("Symbol", render_kw={'readonly': True})
+    from_amount = FloatField("Amount", render_kw={'readonly': True})
+    convert_to_country = StringField("To", render_kw={'readonly': True})
+    to_currency_code = StringField("Currency Code", render_kw={'readonly': True})
+    to_symbol = StringField("Symbol", render_kw={'readonly': True})
+    to_amount = FloatField("Amount", render_kw={'readonly': True})
