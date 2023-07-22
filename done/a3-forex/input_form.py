2449ef66c30e6c7ef0704428399cdd644f096f16
Yu <yu@pfix.org>
2023-07-21 21:56:15 -0700

    WIP

--- done/a3-forex/input_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import FloatField, SelectField
-from wtforms.validators import InputRequired, NumberRange
-
-class InputForm(FlaskForm):
-    from_currency_code = SelectField("From", validators=[InputRequired()])
-    to_currency_code = SelectField("To", validators=[InputRequired()])
-    from_amount = FloatField("Amount", validators=[InputRequired()])
